aa594ab9e7f6db9037ede0dbdb3181f735af1dad
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-11-11 21:25:24 -0400

    main criado

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Versão sequencial
+ * 
+ * 
+ * @file main.cpp
+ * @author João Víctor Zárate, Julio Huang, Ricardo Abreu
+ * @brief Trabalho 1 - Progrmação Paralela
+ * @version 1.0
+ * @date 2020-11-11
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    
+}
