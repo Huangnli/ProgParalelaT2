11234fe185e9a4e2677f89db9e7f4835d1212fd4
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-11-13 20:28:42 -0400

    tirei escrever arq saida e add tempo de execucao

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #define ARQUIVO_H
 
 #include <iostream>
+#include <sys/time.h>
 
 #include "Arquivo.h"
 #include "CalculoDE.h"
@@ -23,21 +24,30 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     //Verifica se todos os parametros foram passados
-    if (argc < 3)
+    if (argc < 2)
     {
-        cout << "Erro! Deve-se passar 3 argumentos:\n";
+        cout << "Erro! Deve-se passar 2 argumentos:\n";
         exit(1);
     }
 
     Arquivo arq_entrada(argv[1], 'r'); //Cria um objeto e abre o arquivo passado como parametro
     arq_entrada.Ler();                 //Le os caracteres do arquivo de entrada
 
+    struct timeval inicio, fim;
+	gettimeofday(&inicio, 0);
+
     CalculoDE calDE(arq_entrada.buffer);
     calDE.menorQuantEdicao();  //Armazena o menor distancia de edicao em uma string
 
+    gettimeofday(&fim, 0);
+    unsigned long microsegundos = fim.tv_usec - inicio.tv_usec;
     //Imprimir menor distancia de edicao
+    /*
     Arquivo arq_saida(argv[2], 'w');
     arq_saida.Escrever(calDE.menorDistEdicao);
+    */
+    printf("%s\n", calDE.menorDistEdicao.c_str());
+    printf("Tempo de execução = %.2lums\n", microsegundos);
 
     exit(0);
 }
